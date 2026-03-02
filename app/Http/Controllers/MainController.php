8bb9ec27faeef3966ac8af241744e8f1498f7a92
neverlaaazy <cheserega174@gmail.com>
2026-03-02 12:55:27 +0500

    exp

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -20,19 +20,24 @@ class MainController extends Controller
             ['id' => 8, 'title' => 'sfm', 'price' => 700,'path' => 'sfm.jpg'],
             ['id' => 9, 'title' => 'gimp', 'price' => 10000,'path' => 'gimp.png']
         ];
+    //рубика эээээксперименты ----------------------------------
+    public $cars = [
+            ['id' => 1, 'carnumber' => 'А 152 КВ 777', 'description' => 'Москва','creationdate' => '15.03.1998'],
+            ['id' => 2, 'carnumber' => 'М 890 ОР 199', 'description' => 'Москва','creationdate' => '22.07.2001'],
+            ['id' => 3, 'carnumber' => 'Х 005 ТМ 99', 'description' => 'Москва','creationdate' => '05.11.2005'],
+            ['id' => 4, 'carnumber' => 'У 412 РВ 178', 'description' => 'С-Петербург','creationdate' => '10.02.2008'],
+            ['id' => 5, 'carnumber' => 'В 733 ЕН 125', 'description' => 'Приморский край','creationdate' => '30.09.2014'],
+            ['id' => 6, 'carnumber' => 'К 911 АА 74', 'description' => 'Челябинская обл.','creationdate' => '12.06.2011'],
+            ['id' => 7, 'carnumber' => 'О 228 СМ 161', 'description' => 'Ростовская обл.','creationdate' => '20.08.2017'],
+            ['id' => 8, 'carnumber' => 'Н 654 УУ 102', 'description' => 'Башкортостан','creationdate' => ' 14.04.2003'],
+            ['id' => 9, 'carnumber' => 'Т 099 ХЕ 197', 'description' => 'Москва','creationdate' => '19.11.2006']
+        ];
 
-    // public $cars = [
-    //         ['id' => 1, 'carnumber' => 'А 152 КВ 777', 'description' => 'Москва','creationdate' => '15.03.1998'],
-    //         ['id' => 2, 'carnumber' => 'М 890 ОР 199', 'description' => 'Москва','creationdate' => '22.07.2001'],
-    //         ['id' => 3, 'carnumber' => 'Х 005 ТМ 99', 'description' => 'Москва','creationdate' => '05.11.2005'],
-    //         ['id' => 4, 'carnumber' => 'У 412 РВ 178', 'description' => 'С-Петербург','creationdate' => '10.02.2008'],
-    //         ['id' => 5, 'carnumber' => 'В 733 ЕН 125', 'description' => 'Приморский край','creationdate' => '30.09.2014'],
-    //         ['id' => 6, 'carnumber' => 'К 911 АА 74', 'description' => 'Челябинская обл.','creationdate' => '12.06.2011'],
-    //         ['id' => 7, 'carnumber' => 'О 228 СМ 161', 'description' => 'Ростовская обл.','creationdate' => '20.08.2017'],
-    //         ['id' => 8, 'carnumber' => 'Н 654 УУ 102', 'description' => 'Башкортостан','creationdate' => ' 14.04.2003'],
-    //         ['id' => 9, 'carnumber' => 'Т 099 ХЕ 197', 'description' => 'Москва','creationdate' => '19.11.2006']
-    //     ];
-
+    public function showCars(){
+        $reports = $this->cars;
+        return view('reports', compact('reports'));
+    }
+    //-----------------------------------------------------------
 
     public function show1(){
         $a = $this->a;
